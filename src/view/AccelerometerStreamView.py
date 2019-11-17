3bbaed75871f2b3424333aee4b762d2e3b0a2f13
AdamVD <avdonle@gmail.com>
2019-11-17 10:39:51 -0500

    Add graphing thread to accel view

--- src/view/AccelerometerStreamView.py
+++ src/view/AccelerometerStreamView.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from collections import deque
 from datetime import time
 from typing import Dict, Deque, Union
+import threading
 
 from src.model.AccelerometerStreamObserver import AccelerometerStreamObserver
 from src.model.AccelerometerStreamSubject import AccelerometerStreamSubject
@@ -18,6 +19,8 @@ class AccelerometerStreamView(QWidget):
     """
     View for displaying accelerometer data.
     """
+
+    lock = threading.Lock()
 
     def __init__(self, accel_stream: AccelerometerStreamSubject, parent=None):
         super().__init__(parent=parent)
@@ -49,20 +52,35 @@ class AccelerometerStreamView(QWidget):
         self.accel_stream = accel_stream
         self.accel_stream.attach(self)
 
+        threading.Thread(daemon=True, target=self.draw_graph).start()
+
     def update(self, x: float, y: float, z: float, timestamp: time) -> None:
-        self.data["x"].append(x)
-        self.data["y"].append(y)
-        self.data["z"].append(z)
-        self.data["time"].append(
-            "{}:{}".format(timestamp.second, timestamp.microsecond)
-        )
-        self.draw_graph()
+        """
+        This is the stream reader thread!! Don't do any long operations or it will drop packets.
+        """
+        plot_timestamp = "{}:{}".format(timestamp.second, timestamp.microsecond)
+
+        # these dequeue operations should already be thread safe - the lock is to ensure
+        # the x, y, z, and time values are updated at the same time before another draw occurs
+        with self.lock:
+            self.data["x"].append(x)
+            self.data["y"].append(y)
+            self.data["z"].append(z)
+            self.data["time"].append(plot_timestamp)
+        # self.draw_graph()  - DON'T DO THIS HERE
 
     def draw_graph(self):
+        """
+        TODO this is really slow. Performs at <10 FPS. Look into matplotlib animation using "blit"
+        (only re-draws the parts of the figure that have been updated) OR use an entirely different
+        plotting library.
+        """
         self.figure.clear()
         subplot = self.figure.add_subplot(111)
-        subplot.plot("time", "x", data=self.data)
-        subplot.plot("time", "y", data=self.data)
-        subplot.plot("time", "z", data=self.data)
+        with self.lock:
+            subplot.plot("time", "x", data=self.data)
+            subplot.plot("time", "y", data=self.data)
+            subplot.plot("time", "z", data=self.data)
         subplot.axes.get_xaxis().set_visible(False)
         self.canvas.draw()
+        self.draw_graph()  # constantly re-draw the graph to achieve a live display
