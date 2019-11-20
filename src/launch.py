02f51d3807660b291bfcd3c8a25cf949487c59da
AdamVD <avdonle@gmail.com>
2019-11-20 00:34:55 -0500

    Rename view package to view_controller

--- src/launch.py
+++ src/launch.py
@@ -3,7 +3,7 @@ import sys
 
 import src.model.AccelerometerStreamReader
 
-import src.view.AccelerometerStreamView
+import src.view_controller.AccelerometerStreamView
 
 import src.test.accelerometer_stream_generator
 
@@ -14,7 +14,7 @@ src.test.accelerometer_stream_generator.run_generator("127.0.0.1", 13500, 60)
 
 app = PyQt5.QtWidgets.QApplication(sys.argv)
 
-gui = src.view.AccelerometerStreamView.AccelerometerStreamView(reader)
+gui = src.view_controller.AccelerometerStreamView.AccelerometerStreamView(reader)
 gui.show()
 
 sys.exit(app.exec_())
